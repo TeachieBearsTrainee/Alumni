d2ede20f3dfd1d6e2f08d6e1c2223f4035fbb7d6
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-03-20 15:10:57 +0530

    fix: added a dummy file in public as git dont accept empty folders try 3

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,15 +1,33 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "./public/temp")
+//     },
+//     filename: function (req, file, cb) {
+      
+//       cb(null, file.originalname)
+//     }
+//   })
+  
+// export const upload = multer({ 
+//     storage, 
+// })
+
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        cb(null, path.join(__dirname, '..', 'public', 'temp')); 
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+        cb(null, file.originalname);
     }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+});
+
+export const upload = multer({ storage });
